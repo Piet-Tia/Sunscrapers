09be7bb113e779b6e8df4ec0f0c5d57d6bc3cfe2
Piotr Nastaj <piotr@nastaj.net>
2019-04-16 14:52:30 +0200

    Add onClick to list of companies

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,23 +6,34 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			searchResults: []
+			searchResults: [],
+			selectedCompanyData: {}
 		};
 		this.handleSearchResults = this.handleSearchResults.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	handleSearchResults(searchResults) {
-		console.log('searchResults: ',searchResults);
-		this.setState({searchResults}) 
+		console.log('searchResults: ', searchResults);
+		this.setState({ searchResults })
+	}
+
+	handleClick(event, element) {
+		console.log('element: ', element);
+		const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&keywords=${element}&apikey=X7RBKGWVBM5RZ6F7`;
+		fetch(url)
+			.then(response => response.json())
+			.then(responseJson => this.setState({responseJson}))
+			.then(console.log(this.state))
 	}
 
 	render() {
 		return (<div>
-		<Search onSearchResults={this.handleSearchResults}/>
+			<Search onSearchResults={this.handleSearchResults} />
 			<ul>
-			{this.state.searchResults.map(elem => <li>{elem['1. symbol']} </li>)}
+				{this.state.searchResults.map((elem,ind) => <li key={ind} onClick={(e) => this.handleClick(e, elem)}>{elem['2. name']} </li>)}
 			</ul>
-			</div>
+		</div>
 		);
 	}
 }
